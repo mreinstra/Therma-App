ab4c012cc931497881596de6d341c8ad52160545
Meg Reinstra <mhreinstra@gmail.com>
2019-02-26 11:42:47 -0800

    initial commit

---
+++ Therma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Therma
+//
+//  Created by Meg Reinstra on 2/26/19.
+//  Copyright © 2019 Meg Reinstra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
