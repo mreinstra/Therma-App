6f95a2ea75097d6479d7d08e7f1dbbd1048cabf1
Charlie Donnelly <charliedonnelly@Charlies-MacBook-Pro-7.local>
2019-03-05 13:46:15 -0800

    This doesnt work but we are trying.

--- Therma/SafetyTalksTableViewController.swift
+++ Therma/SafetyTalksTableViewController.swift
@@ -8,30 +8,70 @@
 
 import UIKit
 
-class SafetyTalksTableViewController: UITableViewController {
-
+class SafetyTalksTableViewController: UITableViewController{
+   
+    @IBOutlet weak var label: UIView!
+    
+    
+    var items = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let item1 = "go get Fish and Chips"
+        items.append(item1)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    // MARK: - TABLE VIEW DELEGATE
+    // tableview(numberOfRowsInSecgion: 1)
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return items.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        
-        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell( withIdentifier: "pdf", for : indexPath)
+        let label = cell.viewWithTag(10) as! UILabel
+        let item = items[indexPath.row]
+        label.text = item
+        //        if item.checked {
+        //            cell.accessoryType = .checkmark
+        //        } else {
+        //            cell.accessoryType = .none
+        //
+        //        }
+        tableView.deselectRow(at: indexPath, animated: true)
         return cell
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+    
+    // MARK:- Table View Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if let cell = tableView.cellForRow(at: indexPath)
+        {
+            let item = items[indexPath.row]
+            // if cell.accessoryType == .none {
+            //    cell.accessoryType = .checkmark
+            // } else {
+            //  cell.accessoryType = .none
+            // }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    
+    override func tableView( _ tableView: UITableView, commit editingStyle:
+        UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        items.remove(at: indexPath.row)
+        let  indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
     }
-
+    
+    
+    
 }
+
+
+
+
